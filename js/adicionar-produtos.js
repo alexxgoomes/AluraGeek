6e01aa92931ce9bea05c7004929dd9171e396d4f
Alex Gomes <alexsgomes0704@gmail.com>
2023-05-17 12:02:24 -0300

    Update adicionar-produtos.js

--- js/adicionar-produtos.js
+++ js/adicionar-produtos.js
@@ -1,7 +1,3 @@
-import checaItens from "./checar-duplicado.js";
-
-let listaDeItens = [];
-
 const form = document.getElementById("form-produto");
 const urlDoProduto = document.getElementById("url");
 const categoriaDoProduto = document.getElementById("categoria");
@@ -9,13 +5,16 @@ const nomeDoProduto = document.getElementById("nome-do-produto");
 const precoDoProduto = document.getElementById("preco");
 const descricaoDoProduto = document.getElementById("descricao");
 
-const checarOsProdutos = window.localStorage.getItem("produtos");
-console.log(checarOsProdutos);
+const checarOsProdutos = JSON.parse(localStorage.getItem("produtos"));
+
+let listaDeItens = [];
 
 form.addEventListener("submit", evento => {
     evento.preventDefault();
     salvarProduto();
 })
+
+
 
 function salvarProduto() {
     const urlProduto = urlDoProduto.value;
@@ -24,10 +23,7 @@ function salvarProduto() {
     const precoProduto = precoDoProduto.value;
     const descricaoProduto = descricaoDoProduto.value;
 
-    
-
-    const checarDuplicado = checarOsProdutos.some((elemento) => elemento.nome.toUpperCase() === nomeProduto.toUpperCase());
-    console.log(checarDuplicado);
+    const checarDuplicado = checarOsProdutos.some((elemento) => elemento.toUpperCase() === nomeProduto.toUpperCase());
 
     if(checarDuplicado) {
         alert("Este item já foi adicionado")
@@ -41,5 +37,41 @@ function salvarProduto() {
         };
     }
 
-    console.log(listaDeItens);
+    localStorage.setItem("itemNovo", JSON.stringify(listaDeItens));
+    window.location.href = "./produtos.html"
+
+    const produtoNovo = document.getElementById("produtos");
+    console.log(produtoNovo);
 }
+
+
+
+{/*function AdicionarProduto() {
+    const urlProduto = urlDoProduto.value;
+    const categoriaProduto = categoriaDoProduto.value;
+    const nomeProduto = nomeDoProduto.value;
+    const precoProduto = precoDoProduto.value;
+    const descricaoProduto = descricaoDoProduto.value;
+
+    listaDeItens ={
+        url: urlProduto,
+        categoria: categoriaProduto,
+        nome: nomeProduto,
+        preco: precoProduto,
+        descricao: descricaoProduto
+    };
+
+    produtoNovo.innerHTML = `
+        <div class="item">
+            <a href="#" class="topicos__imagem__link">
+                <div class="lixeira">
+                    <img class="produto__lixeira" src="./img/Lixeira.svg" alt="Lixeira">
+                </div>
+                <img src="./img/Kylo-ren.svg" alt="Boneco do Kylo Ren" class="topicos__imagem">
+            </a>
+            <p class="topicos__descricao">Kylo Ren</p>
+            <p class="topicos__valor">R$ 60,00</p>
+            <a href="#" class="topicos__link">Ver produto</a>
+        </div>
+     `
+}*/}
