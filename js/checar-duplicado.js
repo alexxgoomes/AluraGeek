b0bb430ff49d6d94cd96bd2627a83290f5d8e62a
Alex Gomes <alexsgomes0704@gmail.com>
2023-05-23 18:18:25 -0300

    Update checar-duplicado.js

--- js/checar-duplicado.js
+++ js/checar-duplicado.js
@@ -1,12 +1,14 @@
-    const itens = document.querySelectorAll(".topicos__descricao");
+    const itens = document.querySelectorAll(".item");
     const seletor = document.querySelector("[data-adicionar]");
     let descricao = [];
 
     const adicionar  = seletor.addEventListener("submit", (e) => {
         e.preventDefault();
         
-        itens.forEach((item) => descricao.push(item.innerHTML));
-    
+        itens.forEach((item) => {
+            descricao.push(item.innerHTML);
+        });
+        
         localStorage.setItem("produtos", JSON.stringify(descricao));
         window.location.href = "./adicionar-produtos.html";
     });
