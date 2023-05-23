58334b5025a640d0da999f918bb3cfc8f65f405b
Alex Gomes <alexsgomes0704@gmail.com>
2023-05-23 18:18:18 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,11 +1,21 @@
 const camposDoFormulario = document.querySelectorAll('[required');
-const formulario = document.querySelector("[data-form]");
+let formulario = [];
+formulario = document.querySelector("[data-form]");
 
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
+function enviarFormulario() {
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-    window.location.href = './index.html';
-})
+        listaDoFormulario = {
+            "email": e.target.elements["email"].value,
+            "senha": e.target.elements["senha"].value
+        }
+
+        localStorage.setItem("login", JSON.stringify(listaDoFormulario));
+        window.location.href = './index.html';
+    });
+}
+enviarFormulario();
 
 camposDoFormulario.forEach(campo => {
     campo.addEventListener("blur", () => verificaCampo(campo));   
