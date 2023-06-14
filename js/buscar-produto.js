d53067435e3a48643134c123da8ec9ee04bf6d02
Alex Gomes <alexsgomes0704@gmail.com>
2023-06-14 15:19:02 -0300

    Update buscar-produto.js

--- js/buscar-produto.js
+++ js/buscar-produto.js
@@ -1,6 +1,22 @@
-import { produtoService } from "./fetch.js";
+import {produtoService} from "./fetch.js";
+import mostraLinha from "./mostrar-produto.js";
 
-async function buscarProduto() {
+async function buscarProduto(evento) {
+    evento.preventDefault();
+
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
-    const busca = await produtoService.buscarProduto();
+    const busca = await produtoService.buscaProduto(dadosDePesquisa);
+
+    const lista = document.querySelector("[data-lista]");
+
+    while(lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+
+            busca.forEach(elemento => lista.appendChild(
+                mostraLinha(elemento.url, elemento.descricao, elemento.nome, elemento.preco, elemento.id)));
 }
+                
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
+
+botaoDePesquisa.addEventListener("click", evento => buscarProduto(evento));
