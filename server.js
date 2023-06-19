d8289899809e6d2b015b0672c38ffece271fb1f5
Alex Gomes <alexsgomes0704@gmail.com>
2023-06-19 16:26:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const data = require('./db.json')
 const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
 
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
 const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
     _isFake: isProductionEnv
 })
@@ -24,5 +23,4 @@ server.listen(process.env.PORT || 8000, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
